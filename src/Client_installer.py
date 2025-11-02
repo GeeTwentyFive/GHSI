544a832abf99989c4ab6db4a9a8a37dfdfd4a064
Gee <alickoamarko@gmail.com>
2025-11-02 23:33:14 +0100

    WIP: Switching to Python

---
+++ src/Client_installer.py
@@ -0,0 +1,58 @@
+import ctypes
+import winreg
+import os
+import tempfile
+import base64
+import tarfile
+
+
+GAME_FILES_COMPRESSED_DATA = b"" # TODO
+
+
+def show_message_box(msg: str, title: str = ""):
+	ctypes.windll.user32.MessageBoxW(0, msg, title, 0)
+
+def registry_get_string_from_key(root: winreg.HKEYType, path: str, name: str):
+	value = winreg.QueryValueEx(
+		winreg.OpenKey(root, path),
+		name
+	)
+	if value[1] != winreg.REG_SZ:
+		raise Exception(f"ERROR: Value of key {name} at path {path} is not string (REG_SZ)")
+	return value[0]
+
+
+# Prompt user to install Source SDK Base 2006
+os.system("explorer steam://install/215")
+show_message_box("Install 'Source SDK Base 2006' on Steam")
+
+
+# Install Hidden: Source - Enhanced Edition
+
+print("Installing Hidden: Source - Enhanced Edition...")
+
+try:
+	game_files_out_path = tempfile.gettempdir() + "\\_GHSI_game_files.tar.gz"
+	with open(game_files_out_path, "wb") as f:
+		f.write(base64.a85decode(GAME_FILES_COMPRESSED_DATA))
+except Exception as e:
+	input(f"ERROR: Failed to write Hidden: Source - Enhanced Edition compressed data to {game_files_out_path}\n{e}")
+	exit(1)
+
+try:
+	sourcemods_path = registry_get_string_from_key(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam", "SourceModInstallPath")
+except Exception as e:
+	input(f"ERROR: Failed to fetch Sourcemod install path from registry\n{e}")
+	exit(1)
+
+try:
+	with tarfile.open(game_files_out_path) as t:
+		t.extractall(sourcemods_path)
+except Exception as e:
+	input(f"ERROR: Failed to extract Hidden: Source - Enhanced Edition to {sourcemods_path}\n{e}")
+	exit(1)
+
+try: os.remove(game_files_out_path)
+except: pass
+
+show_message_box("Restart Steam to complete installation", "Success!")
