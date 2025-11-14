07b28367a7120da22f657ced575dfc14ecd573bf
Gee <alickoamarko@gmail.com>
2025-11-14 20:35:20 +0100

    V8: Many changes, written in release note

--- README.md
+++ README.md
@@ -2,18 +2,27 @@ GHSI (Gee's Hidden Source Installer) is a program which installs a modified vers
 
 The modifications:
 - Optimized graphical settings for seeing the Hidden
-- Optimized network settings for physkilling and other tricks involving props
+- Optimized network settings for responsiveness, consistency, physkilling, and other tricks involving props
 - Includes many community maps
 - cl_downloadfilter all (to (usually) prevent missing map error when joining a server which is running a map you don't have)
 - Listen server fix
 - Sonic trip alarm sound fix
 - Master server list fix
+- `cl_pred_optimize 0` (to fix recoil)
+- `cl_extrapolate 0` (more accurate player positions*)
+- Automatic (attempted) port forwarding (27015)
+- Shotgun damage nerf (17x9 -> 11x9)
+- Increased FN303 (best weapon) clip size (15 -> 20)
+- Increased sonic trip alarm count (1 -> 4)
+- Removed broken maps which crash game
+- Unofficial community Discord link in MOTD
+- Dedicated server plugins: RTV, nominations, mapchooser, phys crash fix, pigshove, +visible, black hidden checker, BSOD fix, weight reporter
 
 
 # Usage
-Run `GHSI.exe`
+Run `GHSInstaller.exe` -> after install: Launch with `GHSLauncher.exe` on Desktop
 
-For dedicated server: Download `HiddenServer.7z` -> extract -> run `START_SERVER.bat`
+For dedicated server: Download `HiddenServer.7z` -> extract -> run `GHSServerLauncher.exe`
 
 
 # Contributors
---
+++ src/GHSInstaller.py
@@ -0,0 +1,97 @@
+import ctypes
+import winreg
+import os
+import tempfile
+import base64
+import tarfile
+
+
+from _GAME_FILES_COMPRESSED_DATA import GAME_FILES_COMPRESSED_DATA
+from _GAME_LAUNCHER_DATA import GAME_LAUNCHER_DATA
+from _SERVER_BROWSER_FIX_COMPRESSED_DATA import SERVER_BROWSER_FIX_COMPRESSED_DATA
+
+
+def show_message_box(msg: str, title: str = ""):
+	ctypes.windll.user32.MessageBoxW(0, msg, title, 0)
+
+def registry_get_string_from_key(root: winreg.HKEYType, path: str, name: str):
+	value = winreg.QueryValueEx(
+		winreg.OpenKey(root, path),
+		name
+	)
+	if value[1] != winreg.REG_SZ:
+		raise Exception(f"ERROR: Value of key {name} at path {path} is not string (REG_SZ)")
+	return value[0]
+
+
+# Prompt user to install Source SDK Base 2006
+os.system("explorer steam://install/215")
+show_message_box("Install 'Source SDK Base 2006' on Steam\n\nPRESS 'OK' ONLY *AFTER* IT IS ALREADY INSTALLED")
+
+
+# Install Hidden: Source - Enhanced Edition
+
+print("Installing Hidden: Source - Enhanced Edition...")
+
+try:
+	game_files_out_path = tempfile.gettempdir() + "\\_GHSI_game_files.tar.gz"
+	with open(game_files_out_path, "wb") as f:
+		f.write(base64.b64decode(GAME_FILES_COMPRESSED_DATA))
+except Exception as e:
+	input(f"ERROR: Failed to write Hidden: Source - Enhanced Edition compressed data to {game_files_out_path}\n{e}")
+	exit(1)
+
+try:
+	sourcemods_path = registry_get_string_from_key(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam", "SourceModInstallPath")
+except Exception as e:
+	input(f"ERROR: Failed to fetch Sourcemod install path from registry\n{e}")
+	exit(1)
+
+try:
+	with tarfile.open(game_files_out_path, "r:gz") as t:
+		t.extractall(sourcemods_path)
+except Exception as e:
+	input(f"ERROR: Failed to extract Hidden: Source - Enhanced Edition to {sourcemods_path}\n{e}")
+	exit(1)
+
+try:
+	with open(os.getenv("userprofile")+"\\Desktop\\GHSLauncher.exe", "wb") as f:
+		f.write(base64.b64decode(GAME_LAUNCHER_DATA))
+except Exception as e:
+	input(f"ERROR: Failed to write GHSLauncher.exe to Desktop\n{e}")
+	exit(1)
+
+
+# Patch Source SDK Base 2006 server browser
+
+print("Patching Source SDK Base 2006 server browser...")
+
+try:
+	server_browser_fix_out_path = tempfile.gettempdir() + "\\_GHSI_server_browser_fix.tar.gz"
+	with open(server_browser_fix_out_path, "wb") as f:
+		f.write(base64.b64decode(SERVER_BROWSER_FIX_COMPRESSED_DATA))
+except Exception as e:
+	input(f"ERROR: Failed to write server browser fix compressed data to {server_browser_fix_out_path}\n{e}")
+	exit(1)
+
+try:
+	steam_path = registry_get_string_from_key(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam", "SteamPath")
+except Exception as e:
+	input(f"ERROR: Failed to fetch Steam path from registry\n{e}")
+	exit(1)
+source_sdk_base_2006_path = steam_path + "\\steamapps\\common\\Source SDK Base"
+
+try:
+	with tarfile.open(server_browser_fix_out_path, "r:gz") as t:
+		t.extractall(source_sdk_base_2006_path)
+except Exception as e:
+	input(f"ERROR: Failed to extract server browser fix to {source_sdk_base_2006_path}\n{e}")
+	exit(1)
+
+
+try: os.remove(game_files_out_path)
+except: pass
+try: os.remove(server_browser_fix_out_path)
+except: pass
+
+show_message_box("Restart Steam to complete installation, then use 'GHSLauncher.exe' (on your Desktop)", "Success!")
